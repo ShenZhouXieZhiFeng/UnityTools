807a3a04ef62235aa920a3ad1a7045850f822231
Stone <827443469@qq.com>
2017-11-29 09:20:51 +0800

    修改目录结构

---
+++ Assets/CountTime/Demo/CountTimeUseDemo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StoneTools;
+
+public class CountTimeUseDemo : MonoBehaviour {
+
+    CountTimeModel model1;
+    void Start () {
+        //计时5秒
+        model1 = CountTime.Count(5)
+        .OnBegin(delegate
+        {
+            Debug.Log("开始");
+        })
+        .OnEnd(delegate {
+            Debug.Log("结束");
+        })
+        .OnStop(delegate {
+            Debug.Log("停止");
+        })
+        //更新，可以指定更新频率
+        .OnUpdate(onTimeUpdate);
+
+        model1.Begin();
+        model1.Pause();
+        model1.Continue();
+        model1.Stop();
+    }
+    // update对应的回调需要有一个float参数
+    void onTimeUpdate(float time)
+    {
+        Debug.Log(time);
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("begin1 5 realtime")) {
+            model1.Begin();
+        }
+        if (GUILayout.Button("stop1"))
+        {
+            model1.Stop();
+        }
+        if (GUILayout.Button("Pause1"))
+        {
+            model1.Pause();
+        }
+        if (GUILayout.Button("Continue"))
+        {
+            model1.Continue();
+        }
+    }
+
+   
+
+}
---
+++ Assets/DrawCanvas/DrawDemo.cs
@@ -0,0 +1,19 @@
+// ========================================================
+// 描 述：
+// 作 者：xzf 
+// 创建时间：2017/11/28 18:05:34
+// 版 本：v 1.0
+// ========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawDemo : MaskableGraphic {
+
+    protected override void OnPopulateMesh(Mesh m)
+    {
+        
+    }
+
+}
